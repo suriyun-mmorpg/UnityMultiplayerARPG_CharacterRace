561d1c1dd351d14cce2c33b83663a9e9e1f1819b
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2019-12-26 09:09:41 +0700

    Add header

--- Scripts/BaseCharacterEntity_Race.cs
+++ Scripts/BaseCharacterEntity_Race.cs
@@ -1,7 +1,10 @@
-﻿namespace MultiplayerARPG
+﻿using UnityEngine;
+
+namespace MultiplayerARPG
 {
     public partial class BaseCharacterEntity
     {
+        [Header("Race Extension Settings")]
         public CharacterRace race;
     }
 }
